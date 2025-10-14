8e40a3fde2919a3ad5b89eabcd64d0868964907c
Natalia <nataliafedorjakova@protonmail.com>
2025-10-14 17:20:16 +0200

    Preparing for dark/light modes

---
+++ src/types/theme.ts
@@ -0,0 +1,13 @@
+import { colors } from '@/styles/colors';
+import { fonts } from '@/styles/fonts';
+
+export const theme = {
+	light: {
+		colors: colors.light,
+		fonts,
+	},
+	dark: {
+		colors: colors.dark,
+		fonts,
+	},
+};
